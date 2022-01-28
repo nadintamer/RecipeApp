2b7c245f78169becc75feab184e71520ab561cb1
Nadin Tamer <nadin.tamer@gmail.com>
2022-01-28 14:41:36 -0800

    made milestone 1 pretty

--- App/Components/RecipeDetail.js
+++ App/Components/RecipeDetail.js
@@ -12,16 +12,49 @@ import {
 
 const categoryURL = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken';
 const URL = 'https://www.themealdb.com/api/json/v1/1/random.php';
+const spaghettiURL = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52770';
 
 export default function RecipeDetail(){
     const [loading, setLoading] = useState(true);
     const [recipes, setRecipes] = useState(false);
 
+		useEffect(() => {
+			fetch(spaghettiURL)
+				.then((response) => response.json())
+				.then((responseJson) => {
+					let spaghetti = responseJson.meals[0];
+					let ingredients = [];
+					let measures = [];
+
+					for (let i = 1; i <= 12; i++) {
+						let ingredient = spaghetti["strIngredient" + i];
+						let measure = spaghetti["strMeasure" + i];
+						ingredients.push(ingredient);
+						measures.push(measure);
+					}
+
+					console.log(ingredients.join("\n"));
+					console.log(measures.join("\n"));
+
+					setLoading(false);
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		})
+
 		return (
 			<SafeAreaView style={styles.container}>
-				<Image style={styles.image} source={{uri: "https:\/\/www.themealdb.com\/images\/media\/meals\/wvpsxx1468256321.jpg"}} />
-				<Text style={styles.title}>Teriyaki Chicken Casserole</Text>
-				<Text style={styles.paragraph}>{decode("Preheat oven to 350\u00b0 F. Spray a 9x13-inch baking pan with non-stick spray.\r\nCombine soy sauce, \u00bd cup water, brown sugar, ginger and garlic in a small saucepan and cover. Bring to a boil over medium heat. Remove lid and cook for one minute once boiling.\r\nMeanwhile, stir together the corn starch and 2 tablespoons of water in a separate dish until smooth. Once sauce is boiling, add mixture to the saucepan and stir to combine. Cook until the sauce starts to thicken then remove from heat.\r\nPlace the chicken breasts in the prepared pan. Pour one cup of the sauce over top of chicken. Place chicken in oven and bake 35 minutes or until cooked through. Remove from oven and shred chicken in the dish using two forks.\r\n*Meanwhile, steam or cook the vegetables according to package directions.\r\nAdd the cooked vegetables and rice to the casserole dish with the chicken. Add most of the remaining sauce, reserving a bit to drizzle over the top when serving. Gently toss everything together in the casserole dish until combined. Return to oven and cook 15 minutes. Remove from oven and let stand 5 minutes before serving. Drizzle each serving with remaining sauce. Enjoy!")}</Text>
+				<View style={styles.logoContainer}>
+					<Text style={styles.logoText}>nomnoms</Text>
+				</View>
+				<View style={styles.imageContainer}>
+					<Image style={styles.image} source={{ uri: "https:\/\/www.themealdb.com\/images\/media\/meals\/sutysw1468247559.jpg" }} />
+				</View>
+				<View style={styles.recipeContainer}>
+					<Text style={styles.title}>Spaghetti Bolognese 🍝</Text>
+					<Text style={styles.recipe}>{`Put the onion and oil in a large pan and fry over a fairly high heat for 3-4 mins. Add the garlic and mince and fry until they both brown. Add the mushrooms and herbs, and cook for another couple of mins.\r\n\r\nStir in the tomatoes, beef stock, tomato ketchup or pur\u00e9e, Worcestershire sauce and seasoning. Bring to the boil, then reduce the heat, cover and simmer, stirring occasionally, for 30 mins.\r\n\r\nMeanwhile, cook the spaghetti in a large pan of boiling, salted water, according to packet instructions. Drain well, run hot water through it, put it back in the pan and add a dash of olive oil, if you like, then stir in the meat sauce. Serve in hot bowls and hand round Parmesan cheese, for sprinkling on top. Enjoy!`}</Text>
+				</View>
 			</SafeAreaView>
 		)
   }
@@ -31,28 +64,58 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		width: '100%',
-		backgroundColor: 'white',
+		backgroundColor: '#66e39e',
 		justifyContent: 'center',
+	},
+	logoContainer: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	logoText: {
+		width: '100%',
+		textAlign: 'center',
+		color: 'white',
+		fontSize: 30,
+		fontWeight: 'bold',
+		textShadowColor:'seagreen',
+    textShadowOffset: { width: 4, height: 2 },
+    textShadowRadius: 0,
+	},
+	imageContainer: {
+		flex: 10,
+		margin: 15,
 	},
 	image: {
 		flex: 1,
+		borderRadius: 20,
+		borderWidth: 3,
+		borderColor: 'white',
 	},
   activityIndicator: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: 80
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 80
   },
   title: {
   	textAlign: 'center',
   	fontSize: 20,
+		fontWeight: 'bold',
   	marginBottom: 10,
   },
-	paragraph: {
-		flex: 1,
+	recipeContainer: {
+		flex: 11,
+		justifyContent: 'center',
+		marginHorizontal: 15,
+		paddingHorizontal: 20,
+		backgroundColor: '#daf0e4',
+		borderRadius: 20,
+	},
+	recipe: {
 		textAlign: 'left',
-		fontSize: 12,
-		margin: 5,
+		fontSize: 13,
+		color: '#4f4f4f'
 	}
 });
 
